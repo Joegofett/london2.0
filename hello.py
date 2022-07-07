f05844d29acaafe2545720403814887f73899411
joegofett <joegofett@system76-pc.localdomain>
2022-07-07 16:31:12 -0500

    got tired so stopping for the day

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
+import http
 from flask import Flask
-
+from discord_bot import translator
+from flask import Request
 app =Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<h1>London 2.0</h1>"
+
+
+
+@app.route('/groupme')
+def groupme():
+    Request()
